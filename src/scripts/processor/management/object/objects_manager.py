47397654feba4eda564acc26eee64c14d7f03250
AMZ <ahmedzaki.bm@gmail.com>
2022-06-09 21:09:47 +0300

    - Created an ObjectsManager

---
+++ src/scripts/processor/management/object/objects_manager.py
@@ -0,0 +1,44 @@
+class ObjectsManager:
+
+    __objects = {}
+
+    @classmethod
+    def create_object(cls, _object, *args, **kwargs):
+
+        try:
+            object_name = _object.__name__
+            repr_name = f"'{object_name}'"
+
+        except AttributeError:
+            print(f"{_object} is not a class!")
+            return None
+
+        try:
+            cls.__objects[object_name] = _object(*args, **kwargs)
+            print("")
+            return cls.__objects[object_name]
+
+        except Exception as E:
+            print(f"Failed to create {repr_name}\n{E}")
+            return None
+
+    @classmethod
+    def delete_object(cls, object_name):
+
+        if object_name not in cls.__objects:
+            print(f"'{object_name}' doesn't exist!")
+            return
+
+        del cls.__objects[object_name]
+
+    @classmethod
+    def detect_clear_memory_leaks(cls):
+        leaks_found = False
+        print("Looking for memory leaks!")
+        for object_name in cls.__objects:
+            leaks_found = True
+            print(f"Found a memory leak! {object_name}")
+            del cls.__objects[object_name]
+
+        if leaks_found:
+            print("Found but cleared memory leaks!")
