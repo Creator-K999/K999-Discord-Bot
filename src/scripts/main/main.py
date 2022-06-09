981984d6c6f86638d0b187316ff3ea2a84eba38b
AMZ <ahmedzaki.bm@gmail.com>
2022-06-09 21:43:21 +0300

    - Started using the logger in main!

--- src/scripts/main/main.py
+++ src/scripts/main/main.py
@@ -1,23 +1,25 @@
 from gc import disable
 
 from main_class import MainClass
+from processor.logger.log import Log
 from processor.management.object.objects_manager import ObjectsManager
 
 
 def main():
     disable()
 
+    Log.debug("Application Started!")
+    Log.debug("Creating 'MainClass'")
     main_object = ObjectsManager.create_object(MainClass)
 
     if main_object is None:
-        print("main_class is None!")
+        Log.critical("main_class is None!")
         return 1
 
     main_object.run()
 
     ObjectsManager.delete_object("MainClass")
     ObjectsManager.detect_clear_memory_leaks()
-    ...
 
 
 if __name__ == '__main__':
