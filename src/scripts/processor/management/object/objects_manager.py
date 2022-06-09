3c1b7371dec49c1535a1f700e64f190809aaee77
AMZ <ahmedzaki.bm@gmail.com>
2022-06-09 21:40:46 +0300

    - Started to use the logger in ObjectsManager class!

--- src/scripts/processor/management/object/objects_manager.py
+++ src/scripts/processor/management/object/objects_manager.py
@@ -1,3 +1,6 @@
+from processor.logger.log import Log
+
+
 class ObjectsManager:
 
     __objects = {}
@@ -12,36 +15,37 @@ class ObjectsManager:
             object_name = _object.__name__
             repr_name = f"'{object_name}'"
 
-        except AttributeError:
-            print(f"{_object} is not a class!")
+        except AttributeError as E:
+            Log.exception(f"{_object} is not a class! {E}")
             return None
 
         try:
             cls.__objects[object_name] = _object(*args, **kwargs)
-            print(f"Successfully created {repr_name}")
+            Log.info(f"Successfully created {repr_name}")
             return cls.__objects[object_name]
 
         except Exception as E:
-            print(f"Failed to create {repr_name}\n{E}")
+            Log.exception(f"Failed to create {repr_name}! {E}")
             return None
 
     @classmethod
     def delete_object(cls, object_name):
 
         if object_name not in cls.__objects:
-            print(f"'{object_name}' doesn't exist!")
+            Log.warning(f"'{object_name}' doesn't exist!")
             return
 
         del cls.__objects[object_name]
+        Log.info(f"Successfully deleted {object_name}")
 
     @classmethod
     def detect_clear_memory_leaks(cls):
         leaks_found = False
-        print("Looking for memory leaks!")
+        Log.debug("Looking for memory leaks...")
         for object_name in cls.__objects:
             leaks_found = True
-            print(f"Found a memory leak! {object_name}")
+            Log.warning(f"Found a memory leak! {object_name}")
             del cls.__objects[object_name]
 
         if leaks_found:
-            print("Found but cleared memory leaks!")
+            Log.warning("Found but cleared memory leaks!")
